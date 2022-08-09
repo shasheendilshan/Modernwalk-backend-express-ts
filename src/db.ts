91148f926e5f21b9c07fd1cce1bcb434e84f9fd1
unknown <shasheen@i-threesixty.com>
2022-08-09 13:30:05 +0530

    Add get tenant by id service and create tenant service

--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,15 @@ import products from "../src/data/products.json";
 import categories from "../src/data/categories.json";
 import tenants from "../src/data/tenants.json";
 import users from "../src/data/users.json";
+import defaultTheme from "../src/data/defaultTheme.json";
 
 
 const data ={
     products,
     categories,
     tenants,
-    users
+    users,
+    defaultTheme
 }
 
 
--- src/routes/tenants.route.ts
+++ src/routes/tenants.route.ts
@@ -1,10 +1,88 @@
 import express, { Request, Response } from "express";
-import data from './../db.js';
+import { readFile, writeFile } from "fs";
+import { v4 as uuidv4 } from "uuid";
+import data from "./../db.js";
+import { ITenant } from "./../Interfaces/tenant.interface";
 
 const tenantsRouter = express.Router();
 
 tenantsRouter.get("/", (req: Request, res: Response) => {
   res.send(data.tenants);
+});
+tenantsRouter.get("/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const tenant: ITenant | undefined = data.tenants.find(
+    (tenant) => tenant.id === id
+  );
+  if (tenant) {
+    const response = {
+      message: "tenant found",
+      data: tenant,
+    };
+    res.send(JSON.stringify(response));
+  } else {
+    const response = {
+      message: "tenant not found",
+      data: null,
+    };
+    res.send(JSON.stringify(response));
+  }
+});
+
+tenantsRouter.post("/", (req: Request, res: Response) => {
+  if (req.body.tenant) {
+    const { name, theme } = req.body.tenant;
+    if (name) {
+      const nameCheck = data.tenants.find((tenant) => tenant.name === name);
+      if (!nameCheck) {
+        const tenant: ITenant = {
+          id: uuidv4(),
+          name: name,
+          theme: theme ? theme : data.defaultTheme,
+        };
+
+        readFile("./dist/data/tenants.json", "utf8", (err, data) => {
+          if (err) throw err;
+          let allTenants = JSON.parse(data);
+          allTenants.push(tenant);
+
+          writeFile(
+            "./src/data/tenants.json",
+            JSON.stringify(allTenants),
+            (err) => {
+              if (err) throw err;
+
+              const response = {
+                message: "Tenant added successfully",
+                data: tenant,
+              };
+
+              res.send(JSON.stringify(response));
+            }
+          );
+        });
+      } else {
+        const response = {
+          message: "Tenant already registered",
+          data: null,
+        };
+
+        res.send(JSON.stringify(response));
+      }
+    } else {
+      const response = {
+        message: "Tenant add Failed",
+        data: null,
+      };
+      res.send(JSON.stringify(response));
+    }
+  } else {
+    const response = {
+      message: "Invalid request",
+      data: null,
+    };
+    res.send(JSON.stringify(response));
+  }
 });
 
 export default tenantsRouter;
