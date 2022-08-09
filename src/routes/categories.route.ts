ff44f3466fd4a9305b352399a18fc2151146aeb3
unknown <shasheen@i-threesixty.com>
2022-08-09 13:32:06 +0530

    Add Code Formatter

--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import data from './../db.js';
+import data from "./../db.js";
 
 const categoriesRouter = express.Router();
 
--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,90 +1,84 @@
 import express, { Request, Response } from "express";
 import { readFile, writeFile } from "fs";
-import {v4 as uuidv4} from 'uuid';
-import data from './../db.js';
-import { IProduct } from './../Interfaces/products.interface';
+import { v4 as uuidv4 } from "uuid";
+import data from "./../db.js";
+import { IProduct } from "./../Interfaces/products.interface";
 
 const productsRouter = express.Router();
 
 productsRouter.get("/", (req: Request, res: Response) => {
   res.send(data.products);
-
 });
 
 productsRouter.get("/:id", (req: Request, res: Response) => {
-   
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const product = data.products.find((prod)=>prod.id===id);
-    if(product){
-       const response ={
-        message:"product found",
-        data:product
-       }
-       res.send(JSON.stringify(response));
-    }else{
-      const response ={
-        message:"product not found",
-        data:null
-       }
-       res.send(JSON.stringify(response));
-    }
-
+  const product = data.products.find((prod) => prod.id === id);
+  if (product) {
+    const response = {
+      message: "product found",
+      data: product,
+    };
+    res.send(JSON.stringify(response));
+  } else {
+    const response = {
+      message: "product not found",
+      data: null,
+    };
+    res.send(JSON.stringify(response));
+  }
 });
 
+productsRouter.post("/", async (req: Request, res: Response) => {
+  if (req.body.product) {
+    const { tenantId, title, price, description, category, image, rating } =
+      req.body?.product;
+    if (tenantId && title && price && description && category && image) {
+      const product: IProduct = {
+        id: uuidv4(),
+        tenantId: tenantId,
+        title: title,
+        price: price,
+        description: description,
+        category: category,
+        image: image,
+        rating: rating,
+      };
 
-productsRouter.post("/", async(req: Request, res: Response) => {
-
-if(req.body.product){
-  const {tenantId,title,price,description,category,image,rating} = req.body?.product;
-  if(tenantId&&title&&price&&description&&category&&image){
-
- const product:IProduct = {
-    id:uuidv4(),
-    tenantId:tenantId,
-    title: title,
-    price: price,
-    description: description,
-    category: category,
-    image: image,
-    rating:rating,
-  }
-  
-  readFile('./dist/data/products.json','utf8', (err, data) => {
-      if (err) throw err;
-      let allProducts = JSON.parse(data);
-      allProducts.push(product);
-
-      writeFile('./src/data/products.json',JSON.stringify(allProducts),(err)=>{
+      readFile("./dist/data/products.json", "utf8", (err, data) => {
         if (err) throw err;
-      })
+        let allProducts = JSON.parse(data);
+        allProducts.push(product);
 
-    })
-    
-    const response ={
-      message :"Product Added successfully",
-      data:product
+        writeFile(
+          "./src/data/products.json",
+          JSON.stringify(allProducts),
+          (err) => {
+            if (err) throw err;
+          }
+        );
+      });
+
+      const response = {
+        message: "Product Added successfully",
+        data: product,
+      };
+
+      res.send(JSON.stringify(response));
+    } else {
+      const response = {
+        message: "Data add Failed",
+        data: null,
+      };
+      res.send(JSON.stringify(response));
     }
-
+  } else {
+    const response = {
+      message: "Invalid request",
+      data: null,
+    };
     res.send(JSON.stringify(response));
-  }else{
-    const response ={
-      message :"Data add Failed",
-      data:null
-    }
-    res.send(JSON.stringify(response));
-
   }
-}else{
-
-  const response ={
-    message :"Invalid request",
-    data:null
-  }
-  res.send(JSON.stringify(response));
-}
-  
-  
 });
 
 export default productsRouter;
