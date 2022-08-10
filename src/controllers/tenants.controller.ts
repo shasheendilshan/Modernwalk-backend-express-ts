1ddfd83176de30160f9b558611c84e15b47e5a93
unknown <shasheen@i-threesixty.com>
2022-08-10 13:26:06 +0530

    Refactor tenants route code and Add tenants controller

---
+++ src/controllers/tenants.controller.ts
@@ -0,0 +1,122 @@
+import { v4 as uuidv4 } from "uuid";
+import data from "./../db.js";
+import { ITenant } from "./../Interfaces/tenant.interface";
+import { fileWriter } from "./../helpers/JsonChange.helper";
+
+export const getAllTenants = () => {
+  return {
+    response: data.tenants,
+    status: 200,
+  };
+};
+
+export const getTenantById = (id: any) => {
+  const tenant: ITenant | undefined = data.tenants.find(
+    (tenant) => tenant.id === id
+  );
+  if (tenant) {
+    const response = {
+      message: "tenant found",
+      data: tenant,
+    };
+    return {
+      response: response,
+      status: 200,
+    };
+  } else {
+    const response = {
+      message: "tenant not found",
+      data: null,
+    };
+    return {
+      response: response,
+      status: 404,
+    };
+  }
+};
+
+export const deleteTenantById = (id: any) => {
+  const tenant: ITenant | undefined = data.tenants.find(
+    (tenant) => tenant.id === id
+  );
+  if (tenant) {
+    const newTenantsList = data.tenants.filter((user) => user?.id !== id);
+
+    fileWriter("./src/data/tenants.json", newTenantsList);
+
+    const response = {
+      message: "Tenant deleted successfully",
+    };
+
+    return {
+      response: response,
+      status: 201,
+    };
+  } else {
+    const response = {
+      message: "Invalid tenant Id",
+    };
+    return {
+      response: response,
+      status: 400,
+    };
+  }
+};
+
+export const addTenant = (tenant: any) => {
+  if (tenant) {
+    const { name, theme } = tenant;
+    if (name) {
+      const nameCheck = data.tenants.find((tenant) => tenant.name === name);
+      if (!nameCheck) {
+        const tenant: ITenant = {
+          id: uuidv4(),
+          name: name,
+          theme: theme ? theme : data.defaultTheme,
+        };
+        let allTenants: ITenant[] = data.tenants;
+
+        allTenants.push(tenant);
+
+        fileWriter("./src/data/tenants.json", allTenants);
+        const response = {
+          message: "Tenant added successfully",
+          data: tenant,
+        };
+
+        return {
+          response: response,
+          status: 201,
+        };
+      } else {
+        const response = {
+          message: "Tenant already registered",
+          data: null,
+        };
+
+        return {
+          response: response,
+          status: 409,
+        };
+      }
+    } else {
+      const response = {
+        message: "Tenant add Failed ",
+        data: null,
+      };
+      return {
+        response: response,
+        status: 500,
+      };
+    }
+  } else {
+    const response = {
+      message: "Invalid request",
+      data: null,
+    };
+    return {
+      response: response,
+      status: 400,
+    };
+  }
+};
--- src/routes/tenants.route.ts
+++ src/routes/tenants.route.ts
@@ -1,117 +1,32 @@
 import express, { Request, Response } from "express";
-import { readFile, writeFile } from "fs";
-import { v4 as uuidv4 } from "uuid";
-import data from "./../db.js";
-import { ITenant } from "./../Interfaces/tenant.interface";
+import {
+  getAllTenants,
+  getTenantById,
+  deleteTenantById,
+  addTenant,
+} from "./../controllers/tenants.controller";
 
 const tenantsRouter = express.Router();
 
 tenantsRouter.get("/", (req: Request, res: Response) => {
-  res.status(200).send(data.tenants);
+  const { status, response } = getAllTenants();
+  return res.status(status).send(response);
 });
 tenantsRouter.get("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
-  const tenant: ITenant | undefined = data.tenants.find(
-    (tenant) => tenant.id === id
-  );
-  if (tenant) {
-    const response = {
-      message: "tenant found",
-      data: tenant,
-    };
-    res.status(200).json(response);
-  } else {
-    const response = {
-      message: "tenant not found",
-      data: null,
-    };
-    res.status(404).json(response);
-  }
+  const { status, response } = getTenantById(id);
+  return res.status(status).send(response);
 });
 tenantsRouter.delete("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
-  const tenantCheck: ITenant | undefined = data.tenants.find(
-    (tenant) => tenant.id === id
-  );
-
-  if (tenantCheck) {
-    const newTenantsList = data.tenants.filter((user) => user?.id !== id);
-
-    writeFile(
-      "./src/data/tenants.json",
-      JSON.stringify(newTenantsList),
-      (err) => {
-        if (err) throw err;
-      }
-    );
-
-    const response = {
-      message: "Tenant deleted successfully",
-    };
-
-    res.status(200).send(response);
-  } else {
-    const response = {
-      message: "Invalid tenant Id",
-    };
-    res.status(400).json(response);
-  }
+  const { status, response } = deleteTenantById(id);
+  return res.status(status).send(response);
 });
 
 tenantsRouter.post("/", (req: Request, res: Response) => {
-  if (req.body.tenant) {
-    const { name, theme } = req.body.tenant;
-    if (name) {
-      const nameCheck = data.tenants.find((tenant) => tenant.name === name);
-      if (!nameCheck) {
-        const tenant: ITenant = {
-          id: uuidv4(),
-          name: name,
-          theme: theme ? theme : data.defaultTheme,
-        };
-
-        readFile("./dist/data/tenants.json", "utf8", (err, data) => {
-          if (err) throw err;
-          let allTenants = JSON.parse(data);
-          allTenants.push(tenant);
-
-          writeFile(
-            "./src/data/tenants.json",
-            JSON.stringify(allTenants),
-            (err) => {
-              if (err) throw err;
-
-              const response = {
-                message: "Tenant added successfully",
-                data: tenant,
-              };
-
-              res.status(201).json(response);
-            }
-          );
-        });
-      } else {
-        const response = {
-          message: "Tenant already registered",
-          data: null,
-        };
-
-        res.status(409).json(response);
-      }
-    } else {
-      const response = {
-        message: "Tenant add Failed",
-        data: null,
-      };
-      res.status(500).json(response);
-    }
-  } else {
-    const response = {
-      message: "Invalid request",
-      data: null,
-    };
-    res.status(400).json(response);
-  }
+  const { tenant } = req.body;
+  const { status, response } = addTenant(tenant);
+  return res.status(status).send(response);
 });
 
 export default tenantsRouter;
