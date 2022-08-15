b69bea158fcc606528237a898e8cd412cdcf694a
unknown <shasheen@i-threesixty.com>
2022-08-15 11:59:28 +0530

    Refactor users route and convert users controller to class component

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -3,147 +3,101 @@ import { fileWriter } from "./../helpers/JsonChange.helper";
 import { IUser } from "./../Interfaces/user.interface";
 import data from "./../db";
 
-export const getAllUsers = (tenantId: any) => {
-  if (tenantId && typeof tenantId == "string") {
-    const users: IUser[] | undefined = data.users.filter(
-      (user: IUser) => user.tenantId === tenantId
-    );
-    if (users.length > 0) {
-      const response = {
-        message: "all users for this tenant",
-        data: users,
-      };
-      return {
-        response: response,
-        status: 200,
-      };
-    } else {
-      const response = {
-        message: "no users for this tenant id",
-        data: users,
-      };
+export default class UsersController {
+  usersData;
 
-      return {
-        response: response,
-        status: 404,
-      };
-    }
-  } else {
-    const response = {
-      message: "Invalid request",
-      data: null,
-    };
-    return {
-      response: response,
-      status: 400,
-    };
+  constructor() {
+    this.usersData = data.users;
   }
-};
-export const getUserById = (tenantId: any, id: string) => {
-  if (id && tenantId) {
-    const user: IUser | undefined = data.users.find(
-      (user: IUser) => user.id === id && user.tenantId === tenantId
-    );
-    if (user) {
-      const response = {
-        message: "user found",
-        data: user,
-      };
 
-      return {
-        response: response,
-        status: 200,
-      };
-    } else {
-      const response = {
-        message: "user not found",
-        data: null,
-      };
-      return {
-        response: response,
-        status: 404,
-      };
-    }
-  } else {
-    const response = {
-      message: "Invalid request",
-      data: null,
-    };
-    return {
-      response: response,
-      status: 400,
-    };
-  }
-};
-
-export const deleteUserById = (tenantId: any, id: string) => {
-  if (id && tenantId) {
-    const user: IUser | undefined = data.users.find(
-      (user: IUser) => user.id === id && user.tenantId === tenantId
-    );
-    if (user) {
-      const newUserList: IUser[] | undefined = data.users.filter(
-        (user: IUser) => user.id !== id && user.tenantId === tenantId
+  public getAllUsers = (tenantId: any) => {
+    if (tenantId && typeof tenantId == "string") {
+      const users: IUser[] | undefined = this.usersData.filter(
+        (user: IUser) => user.tenantId === tenantId
       );
-
-      fileWriter("./src/data/users.json", newUserList);
-
-      const response = {
-        message: "User deleted successfully",
-      };
-
-      return {
-        response: response,
-        status: 201,
-      };
-    } else {
-      const response = {
-        message: "user not found for tenant id or user id",
-        data: null,
-      };
-      return {
-        response: response,
-        status: 404,
-      };
-    }
-  } else {
-    const response = {
-      message: "Invalid request",
-      data: null,
-    };
-    return {
-      response: response,
-      status: 400,
-    };
-  }
-};
-export const addUser = (user: any) => {
-  if (user) {
-    const { firstName, lastName, email, password, tenantId } = user;
-    if (tenantId && firstName && lastName && email && password) {
-      const emailCheck = data.users?.find(
-        (user: IUser) => user.email === email && user.tenantId === tenantId
-      );
-
-      if (!emailCheck) {
-        const user: IUser = {
-          id: uuidv4(),
-          tenantId: tenantId,
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          password: password,
+      if (users.length > 0) {
+        const response = {
+          message: "all users for this tenant",
+          data: users,
+        };
+        return {
+          response: response,
+          status: 200,
+        };
+      } else {
+        const response = {
+          message: "no users for this tenant id",
+          data: users,
         };
 
-        let allUsers: IUser[] = data.users;
+        return {
+          response: response,
+          status: 404,
+        };
+      }
+    } else {
+      const response = {
+        message: "Invalid request",
+        data: null,
+      };
+      return {
+        response: response,
+        status: 400,
+      };
+    }
+  };
 
-        allUsers.push(user);
+  public getUserById = (tenantId: any, id: string) => {
+    if (id && tenantId) {
+      const user: IUser | undefined = this.usersData.find(
+        (user: IUser) => user.id === id && user.tenantId === tenantId
+      );
+      if (user) {
+        const response = {
+          message: "user found",
+          data: user,
+        };
 
-        fileWriter("./src/data/users.json", allUsers);
+        return {
+          response: response,
+          status: 200,
+        };
+      } else {
+        const response = {
+          message: "user not found",
+          data: null,
+        };
+        return {
+          response: response,
+          status: 404,
+        };
+      }
+    } else {
+      const response = {
+        message: "Invalid request",
+        data: null,
+      };
+      return {
+        response: response,
+        status: 400,
+      };
+    }
+  };
+
+  public deleteUserById = (tenantId: any, id: string) => {
+    if (id && tenantId) {
+      const user: IUser | undefined = this.usersData.find(
+        (user: IUser) => user.id === id && user.tenantId === tenantId
+      );
+      if (user) {
+        const newUserList: IUser[] | undefined = this.usersData.filter(
+          (user: IUser) => user.id !== id && user.tenantId === tenantId
+        );
+
+        fileWriter("./src/data/users.json", newUserList);
 
         const response = {
-          message: "user added successfully",
-          data: user,
+          message: "User deleted successfully",
         };
 
         return {
@@ -152,33 +106,88 @@ export const addUser = (user: any) => {
         };
       } else {
         const response = {
-          message: "user already added",
+          message: "user not found for tenant id or user id",
           data: null,
         };
-
         return {
           response: response,
-          status: 409,
+          status: 404,
         };
       }
     } else {
       const response = {
-        message: "user add Failed",
+        message: "Invalid request",
         data: null,
       };
       return {
         response: response,
-        status: 500,
+        status: 400,
       };
     }
-  } else {
-    const response = {
-      message: "Invalid request",
-      data: null,
-    };
-    return {
-      response: response,
-      status: 400,
-    };
-  }
-};
+  };
+  public addUser = (user: any) => {
+    if (user) {
+      const { firstName, lastName, email, password, tenantId } = user;
+      if (tenantId && firstName && lastName && email && password) {
+        const emailCheck = this.usersData?.find(
+          (user: IUser) => user.email === email && user.tenantId === tenantId
+        );
+
+        if (!emailCheck) {
+          const user: IUser = {
+            id: uuidv4(),
+            tenantId: tenantId,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+          };
+
+          let allUsers: IUser[] = this.usersData;
+
+          allUsers.push(user);
+
+          fileWriter("./src/data/users.json", allUsers);
+
+          const response = {
+            message: "user added successfully",
+            data: user,
+          };
+
+          return {
+            response: response,
+            status: 201,
+          };
+        } else {
+          const response = {
+            message: "user already added",
+            data: null,
+          };
+
+          return {
+            response: response,
+            status: 409,
+          };
+        }
+      } else {
+        const response = {
+          message: "user add Failed",
+          data: null,
+        };
+        return {
+          response: response,
+          status: 500,
+        };
+      }
+    } else {
+      const response = {
+        message: "Invalid request",
+        data: null,
+      };
+      return {
+        response: response,
+        status: 400,
+      };
+    }
+  };
+}
--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,35 +1,31 @@
 import express, { Request, Response } from "express";
-import {
-  getAllUsers,
-  getUserById,
-  deleteUserById,
-  addUser,
-} from "./../controllers/users.controller";
+import UsersController from "./../controllers/users.controller";
 
 const usersRouter = express.Router();
+const usersController = new UsersController();
 
 usersRouter.get("/", (req: Request, res: Response) => {
   const { tenantId } = req.query;
-  const { status, response } = getAllUsers(tenantId);
+  const { status, response } = usersController.getAllUsers(tenantId);
   return res.status(status).send(response);
 });
 
 usersRouter.get("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   const { tenantId } = req.query;
-  const { status, response } = getUserById(tenantId, id);
+  const { status, response } = usersController.getUserById(tenantId, id);
   return res.status(status).send(response);
 });
 usersRouter.delete("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   const { tenantId } = req.query;
-  const { status, response } = deleteUserById(tenantId, id);
+  const { status, response } = usersController.deleteUserById(tenantId, id);
   return res.status(status).send(response);
 });
 
 usersRouter.post("/", (req: Request, res: Response) => {
   const { user } = req.body;
-  const { status, response } = addUser(user);
+  const { status, response } = usersController.addUser(user);
   return res.status(status).send(response);
 });
 
